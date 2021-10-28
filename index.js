1e019e730a84b1c5793301f41f073055d20cf73d
drdarian <dariantaban@gmail.com>
2021-10-28 15:02:56 -0700

    building form

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hi there!');
+  res.send(`
+  <div>
+    <h1>Hello</h1>
+    <form method="POST">
+      <input name="email" placeholder="email" />
+      <input name="password" placeholder="password" />
+      <input name="password-confirmation" placeholder="password confirmation" />
+      <button>Sign Up</button>
+    </form>
+  </div>
+  `);
+});
+
+app.post('/', (req, res) => {
+  res.send('Account created!!!')
 });
 
 app.listen(3000, () => {
