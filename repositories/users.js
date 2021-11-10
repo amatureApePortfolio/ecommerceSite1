1bda54dbe68bfba7792eae9cadc0c8f51492613a
drdarian <dariantaban@gmail.com>
2021-11-09 18:03:19 -0800

    update function built

--- repositories/users.js
+++ repositories/users.js
@@ -46,13 +46,25 @@ class UsersRepository {
     const filteredRecords = records.filter(record => record.id !== id);
     await this.writeAll(filteredRecords);
   }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find(record => record.id === id);
+
+    if (!record) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
 }
 
 
 const test = async () => {
   const repo = new UsersRepository('user.json');
 
-  await repo.delete('e6eeeaad');
+  await repo.update('c6707643', { password: 'mypassword' });
 }
 const repo = new UsersRepository('users.json');
 test();
