af598d75f827115466a31ecbdc3bd7735808ddf0
drdarian <dariantaban@gmail.com>
2021-11-10 22:08:46 -0800

    userId displayed

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const usersRepo = require('./repositories/users');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({
+  keys: ['lkajsflaksjdf']
+}));
 
 app.get('/', (req, res) => {
   res.send(`
   <div>
-    <h1>Hello</h1>
+    <p>Your id is: ${req.session.userId}</p>
     <form method="POST">
       <input name="email" placeholder="email" />
       <input name="password" placeholder="password" />
@@ -20,8 +25,21 @@ app.get('/', (req, res) => {
 });
 
 
-app.post('/', (req, res) => {
-  console.log(req.body);
+app.post('/', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send('Email in use')
+  }
+  if (password !== passwordConfirmation) {
+    return res.send('Passwords must match')
+  }
+
+  const user = await usersRepo.create({ email, password });
+
+  req.session.userId = user.id;
+
   res.send('Account created!!!');
 });
 
