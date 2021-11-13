3293b8e3934558606e9e21eee1a1e77edd537060
drdarian <dariantaban@gmail.com>
2021-11-13 13:31:31 -0800

    typo fix

--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -1,5 +1,5 @@
 const { check } = require('express-validator');
-const usersRepo = require('../../repositories');
+const usersRepo = require('../../repositories/users');
 
 module.exports = {
   requireEmail: check('email').trim().normalizeEmail().isEmail().withMessage('Must be valid email').custom(async (email) => {
@@ -9,7 +9,7 @@ module.exports = {
     }
   }),
   requirePassword: check('password').trim().isLength({ min: 4, max: 20 }).withMessage('Must be between 4 and 20 characters'),
-  passwordConfirmation: check('passwordConfirmation').trim().isLength({ min: 4, max: 20 }).withMessage('Must be between 4 and 20 characters').custom((passwordConfirmation, { req }) => {
+  requirePasswordConfirmation: check('passwordConfirmation').trim().isLength({ min: 4, max: 20 }).withMessage('Must be between 4 and 20 characters').custom((passwordConfirmation, { req }) => {
     if (passwordConfirmation !== req.body.password) {
       throw new Error('Passwords must match');
     }
